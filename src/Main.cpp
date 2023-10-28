be7e904e717fa55e950151eed557bf61117dec5b
tarturr <tartur.dev@gmail.com>
2023-10-28 03:23:01 +0200

    Creation of a Main file for users to try out their own entries on the answers to questions

---
+++ src/Main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include "../include/common/Input.hpp"
+#include "../include/common/Questions.hpp"
+#include "../include/2023/1 - Reception des convives/Answer.hpp"
+
+
+int main()
+{
+    constexpr int PROBLEMS_SOLVED{ 1 };
+
+    QuestionManager<PROBLEMS_SOLVED> questions
+    {{
+        std::make_unique<GuestsReception>("Reception des convives", 2023, 1)
+    }};
+
+    std::cout << "==================================================\n\n"
+                 "Welcome to the answers of the Cod'Insa Qualifications\n"
+                 "made by Tartur! You will be able to choose the correction\n"
+                 "you want to test, with your own inputs.\n\n"
+                 "Please consider to set a star on this repository\n"
+                 "if you liked my programs.\n\n"
+                 "==================================================\n" << std::endl;
+
+    auto minMaxYears{ questions.GetMinAndMaxYear() };
+    int year{ Input(
+            "Which year do you want to choose? ["
+            + std::to_string(minMaxYears.first) + "-" + std::to_string(minMaxYears.second) + "]",
+            [&minMaxYears](int n) -> bool
+            {
+                return n >= minMaxYears.first && n <= minMaxYears.second;
+            },
+            "This year has not been implemented yet or does not exist."
+    ) };
+
+    auto minMaxNumbers{ questions.GetMinAndMaxNumber(year) };
+    int number{ Input(
+            "Which year do you want to choose? ["
+            + std::to_string(minMaxNumbers.first) + "-" + std::to_string(minMaxNumbers.second) + "]",
+            [&minMaxNumbers](int n) -> bool
+            {
+                return n >= minMaxNumbers.first && n <= minMaxNumbers.second;
+            },
+            "This year has not been implemented yet or does not exist."
+    ) };
+
+    std::cout << "Let's go for the question N°" << number << " - " << year << " : "
+            << questions.GetQuestion(year, number)->GetName() << " !" << std::endl;
+
+    return EXIT_SUCCESS;
+}
